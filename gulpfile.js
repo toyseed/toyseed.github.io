c0a20df44bd637d510ad16a154f4adea1c410d1b
seoung song <toyseed@gmail.com>
2019-10-14 11:56:45 +0900

    fix css error

--- gulpfile.js
+++ gulpfile.js
@@ -4,42 +4,43 @@ const browserSync = require('browser-sync').create();
 const taggen = require('./__module/taggen');
 
 const messages = {
-    jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build'
+  jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build'
 };
 
-gulp.task('jekyll-build', function(done) {
-    browserSync.notify(messages.jekyllBuild);
-    return cp.spawn('jekyll', ['build', '--drafts'], {
-        stdio: 'inherit'
-    }).on('close', done);
+gulp.task('jekyll-build', function (done) {
+  browserSync.notify(messages.jekyllBuild);
+  return cp.spawn('bundle', ['exec', 'jekyll', 'build', '--drafts'], {
+    // return cp.spawn('jekyll', ['build', '--drafts'], {
+    stdio: 'inherit'
+  }).on('close', done);
 });
 
-gulp.task('browserSync-reload', function(done) {
-    browserSync.reload();
-    done();
+gulp.task('browserSync-reload', function (done) {
+  browserSync.reload();
+  done();
 });
 
-gulp.task('browserSync', function(done) {
-    browserSync.init({
-        server: {
-            baseDir: '_site'
-        }
-        , browser: 'google chrome'
-    });
+gulp.task('browserSync', function (done) {
+  browserSync.init({
+    server: {
+      baseDir: '_site'
+    }
+    , browser: 'google chrome'
+  });
 
-    done();
+  done();
 });
 
 const watchTarget = ["**/*.+(html|scss|md)", "!_site/**/*", "!node_modules/**/*", "!__module/**/*"];
 
-gulp.task('watch', function() {
-    gulp.watch(watchTarget, gulp.series('jekyll-build', 'browserSync-reload'));
+gulp.task('watch', function () {
+  gulp.watch(watchTarget, gulp.series('jekyll-build', 'browserSync-reload'));
 });
 
 gulp.task('serve', gulp.series('jekyll-build', 'browserSync'));
 
 gulp.task('taggen', async function () {
-    taggen(__dirname, 'tag');
+  taggen(__dirname, 'tag');
 });
 
 gulp.task('default', gulp.series('serve', 'watch'));
